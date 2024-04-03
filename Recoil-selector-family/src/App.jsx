3b61e72d626b816e992024f3efa041203a549e8c
Vedant Joshi <vedantj76@gmail.com>
2024-04-03 12:07:14 +0530

    Add files via upload

---
+++ Recoil-selector-family/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import { RecoilRoot, useRecoilState } from 'recoil';
+import { todosAtomFamily } from './atoms';
+
+function App() {
+  return <RecoilRoot>
+   {Array.from({ length: 5 }).map((_, index) => (
+        <div key={index}>{<Todo id={index + 1}/>}</div>
+      ))}
+  </RecoilRoot>
+}
+
+function Todo({id}) {
+   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+
+  return (
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
+  )
+}
+
+export default App
